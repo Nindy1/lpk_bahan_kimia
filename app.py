6874b428c911400b0f89beffb85e4798cc577126
Nindy1 <nindyaghinaramadhani@gmail.com>
2025-07-15 14:37:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "MSDS Bahan Kimia API Aktif"
+
+@app.route('/get-json')
+def get_json():
+    # Baca Excel
+    df = pd.read_excel('msds_bahan_kimia.xlsx')
+    # Convert ke JSON
+    data = df.to_dict(orient='records')
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
